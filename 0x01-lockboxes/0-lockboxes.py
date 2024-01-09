30362fab63372f66244e0806809641a083ebfb09
Anotibills <anotibills@gmail.com>
2024-01-09 09:12:46 +0100

    Task on lockboxes for interview

---
+++ 0x01-lockboxes/0-lockboxes.py
@@ -0,0 +1,17 @@
+"""
+Function that determines all boxes are opened
+"""
+def canUnlockAll(boxes):
+    if not isinstance(boxes, list):
+        return False
+
+    if len(boxes) == 0:
+        return False
+
+    keys = [0]
+    for box_id in keys:
+        for key in boxes[box_id]:
+            if key not in keys and key != box_id and 0 < key < len(boxes):
+                keys.append(key)
+
+    return len(keys) == len(boxes)
