5563b11eab33882eacb0c943a80ff91e35fd62e0
anotibills <anotibills@gmail.com>
2024-03-15 14:05:08 +0100

    Trying this

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -4,28 +4,33 @@ This is the prime game winner determination
 """
 
 
+def generate_primes(n):
+    """ Generate primes up to n """
+    primes = [2]
+    for i in range(3, n + 1, 2):
+        is_prime = True
+        for j in range(3, int(i ** 0.5) + 1, 2):
+            if i % j == 0:
+                is_prime = False
+                break
+        if is_prime:
+            primes.append(i)
+    return primes
+
+
 def isWinner(x, nums):
-    '''
-    This determines the best result
-    '''
+    """
+    This returns the name of the player that won the most rounds
+    If the winner cannot be determined, return None
+    """
+
     score = {"Maria": 0, "Ben": 0}
-    primes = [0, 0, 2]
-    add_prime(max(nums), primes)
+    max_num = max(nums)
+    primes = generate_primes(max_num)
 
-    for round in range(x):
-        _sum=sum((i!=0)*(i<=nums[round])for i in primes[:nums[round]+1])
-        if _sum % 2:
-            winner = "Maria"
-        else:
-            winner = "Ben"
-        if winner == "Maria":
-            score["Maria"] += 1
-        elif winner == "Ben":
-            score["Ben"] += 1
+    for round_num in range(x):
+        round_score = sum(1 for prime in primes if prime <= nums[round_num])
+        winner = "Maria" if round_score % 2 != 0 else "Ben"
+        score[winner] += 1
 
-    if score["Maria"] > score["Ben"]:
-        return "Maria"
-    elif score["Ben"] > score["Maria"]:
-        return "Ben"
-
-    return None
+    return max(score, key=score.get) if score["Maria"] != score["Ben"] else None
