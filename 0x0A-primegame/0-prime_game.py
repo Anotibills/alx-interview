54fde5d589601f35f7cfcceaa21a4bf0ab73879e
anotibills <anotibills@gmail.com>
2024-03-15 04:32:49 +0100

    do over

--- 0x0A-primegame/0-prime_game.py
+++ 0x0A-primegame/0-prime_game.py
@@ -6,31 +6,39 @@ This is the prime game winner determination
 
 def isWinner(x, nums):
     """
-    This determines who the winner is
+    This determine winner of each round and the player who won the most rounds.
     """
     if x < 1 or not nums:
         return None
 
     m_wins = 0
     b_wins = 0
-    
-    #This will generate numbers
-    n = max(nums)
-    primes = [True] * (n + 1)
-    primes[0] = primes[1] = False
 
-    for x in range(2, int(n**0.5) + 1):
-        if primes[x]:
-            for y in range(x**2, n + 1, x):
-                primes[y] = False
-
-    #This will count the numbers of pm less than n i nums
     for n in nums:
-        count = sum(primes[2:n+1])
-        b_wins += count % 2 == 0
-        m_wins += count % 2 == 1
+        if n == 1:
+            b_wins += 1
+            continue
+
+        # Check if Maria can make a move
+        if n % 2 == 0:
+            m_wins += 1
+            continue
+
+        # Check if Ben can make a move
+        for i in range(3, int(n**0.5) + 1, 2):
+            if n % i == 0:
+                b_wins += 1
+                break
+        else:
+            m_wins += 1
 
     if m_wins == b_wins:
         return None
 
     return 'Maria' if m_wins > b_wins else 'Ben'
+
+
+if __name__ == "__main__":
+    x = 3
+    nums = [4, 5, 1]
+    print(isWinner(x, nums))  # Output: Ben
